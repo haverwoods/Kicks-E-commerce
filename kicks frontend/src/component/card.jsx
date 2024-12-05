a43df1503992cb25b8ada6e536a1c2244603a521
haverwoods <shresthasamrat307@gmail.com>
2024-12-05 18:54:06 +0545

    minor bug fix in card

--- kicks frontend/src/component/card.jsx
+++ kicks frontend/src/component/card.jsx
@@ -3,15 +3,15 @@ import React from 'react'
 
 const Card = () => {
   return (
-    <div className="mt-5 mb-5 ml-14 inline-block max-w-sm overflow-hidden ">
-    <div className=" card border-[10px] border-[#FAFAFA] rounded-3xl shadow-lg  overflow-hidden">
+    <div className="rounded-t-3xl  mt-5 mb-5 ml-14 inline-block max-w-sm overflow-hidden ">
+    <div className=" card border-[8px] border-[#FAFAFA] rounded-3xl shadow-lg  overflow-hidden">
       <img
         src="../asset/Rectangle 5.png"
         className="rounded-3xl  object-cover w-full overflow-hidden"
       />
     </div>
     <div className="pt-5">
-      <span className=" block mt-2 text-slate-700 text-xl font-medium break-words overflow-hidden">
+      <span className=" px-3 block mt-2 text-slate-700 text-xl font-medium break-words overflow-hidden">
       Lorem ipsum dolor 
       </span>
       <Button className="  mt-3 px-5 py-5 w-80">
