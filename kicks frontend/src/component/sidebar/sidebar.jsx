4ea8fb24d3adb761cf1f2657b902e12c53e17f07
haverwoods <shresthasamrat307@gmail.com>
2025-01-22 12:22:24 +0545

    added new feature for color

--- kicks frontend/src/component/sidebar/sidebar.jsx
+++ kicks frontend/src/component/sidebar/sidebar.jsx
@@ -15,7 +15,7 @@ const Sidebar = () => {
     }));
   };
 
-  const  getSubLinkCount = (sublinks) => {
+  const getSubLinkCount = (sublinks) => {
     //check is sublink esits if yes then return the length of sublink or else 0
     return sublinks ? sublinks.length : 0;
   };
@@ -44,7 +44,7 @@ const Sidebar = () => {
                   <div className=" ">
                     <div
                       className={`inline-grid ${
-                           //check if length of sublink is more than 8 , if yes then send grid cols 2 or else 1
+                        //check if length of sublink is more than 8 , if yes then send grid cols 2 or else 1
                         getSubLinkCount(link.sublinks) > 8
                           ? "grid-cols-2"
                           : "grid-cols-1"
@@ -52,20 +52,38 @@ const Sidebar = () => {
                     >
                       {link.sublinks.map((sub) => (
                         <div key={sub.name} className="flex items-center">
-                          <div className="flex items-center">
-                            <input
-                              type="checkbox"
-                              id={sub.name}
-                              className="peer hidden"
-                            />
-                            <label
-                              htmlFor={sub.name}
-                              className="w-5 h-5 bg-gray-200 border border-gray-300 rounded-sm flex items-center justify-center peer-checked:bg-black peer-checked:border-black peer-checked:text-white text-transparent"
-                            >
-                              ✓
-                            </label>
-                            <span className="ml-2">{sub.name}</span>
-                          </div>
+                          {/* //check if link is color then show color else show checkbox */}
+                          {link.name === "color" ? (
+                            <div className="flex items-center">
+                              <Button
+                                key={sub.name}
+                                variant="outline"
+                                className={`w-8 h-8 rounded-full`}
+                                //since tailwind does not support linear gradient as bg color so we need to use css
+                                style={{
+                                  background: sub.bgColor,
+                                }}
+                                size="icon"
+                              ></Button>
+                              <p className="ml-2">{sub.name}</p>
+                            </div>
+                          ) : (
+                            //if not color then show checkbox
+                            <div className="flex items-center">
+                              <input
+                                type="checkbox"
+                                id={sub.name}
+                                className="peer hidden"
+                              />
+                              <label
+                                htmlFor={sub.name}
+                                className="w-5 h-5 bg-gray-200 border border-gray-300 rounded-sm flex items-center justify-center peer-checked:bg-black peer-checked:border-black peer-checked:text-white text-transparent"
+                              >
+                                ✓
+                              </label>
+                              <span className="ml-2">{sub.name}</span>
+                            </div>
+                          )}
                         </div>
                       ))}
                     </div>
@@ -73,33 +91,6 @@ const Sidebar = () => {
                 )}
               </div>
             ))}
-          </div>
-        </div>
-
-        <div class="color" className="mt-6">
-          <p className="text-xl font-normal mb-2">color</p>
-          <div className=" inline-grid grid-cols-2 gap-x-10">
-            <div className="inline-grid grid-cols-2 gap-x-3 ">
-              <Button
-                key="red"
-                // key={color.name}
-                variant="outline"
-                className="w-8 h-8 rounded-full bg-[#D2122E] hover:bg-[#fd5c63] hover:text-white hover:ring-2 hover:ring-[#fc2f39]"
-                size="icon"
-              ></Button>
-              <p className="text-lg font-normal">red</p>
-            </div>
-
-            <div className="inline-grid grid-cols-2 gap-x-3 ">
-              <Button
-                key="red"
-                // key={color.name}
-                variant="outline"
-                className="w-8 h-8 rounded-full bg-[#D2122E] hover:bg-[#fd5c63] hover:text-white"
-                size="icon"
-              ></Button>
-              <p className="text-lg font-normal">red</p>
-            </div>
           </div>
         </div>
       </div>
